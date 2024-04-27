4b92e15a2562a61446338449f960339bd0e3c96d
Kewtty <202301647@est.umss.edu>
2024-04-27 00:46:39 -0400

    cree repositorio

---
+++ src/main/java/com/mycompany/calculadoraproyecto/Operaciones.java
@@ -0,0 +1,38 @@
+
+package com.mycompany.calculadoraproyecto;
+
+public class Operaciones {
+
+    double a, b, c;
+    
+    
+//ATRIBUTOS
+
+//CONSTRUCTOR
+
+//METODOS
+public double suma (double a, double b){
+
+    c = a+b;
+    return c;
+}
+
+public double resta (double a, double b){
+
+    c = a-b;
+    return c;
+}
+
+public double mult (double a, double b){
+
+    c = a*b;
+    return c;
+}
+
+public double div (double a, double b){
+
+    c = a/b;
+    return c;
+}
+    
+}
